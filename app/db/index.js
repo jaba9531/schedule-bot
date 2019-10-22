ebcf3f0aef8c2e7f71629efb03bd0b6fd2acb598
Jake Barber <jakecbarber@gmail.com>
2019-10-22 02:02:16 -0600

    Finish writing controller endpoints and notification service

--- app/db/index.js
+++ app/db/index.js
@@ -8,18 +8,15 @@ let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
     console.error(err.message);
   } else {
     console.log('Connected to the events database.');
+    // Create Events Table
     db.serialize(() => {
-      db.all(`CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, name UNIQUE, date);`, (err, row) => {
+      db.all(`CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, name UNIQUE, description, date, notifications);`, (err, row) => {
         if (err) {
           console.error(err.message);
         }
       });
-      db.all(`CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name);`, (err, row) => {
-        if (err) {
-          console.error(err.message);
-        }
-      });
-      db.all(`CREATE TABLE IF NOT EXISTS users_events (username, event_id INTEGER, FOREIGN KEY (event_id) REFERENCES events(id));`, (err, row) => {
+      // Create Users_Events Table
+      db.all(`CREATE TABLE IF NOT EXISTS users_events (username, event_id INTEGER);`, (err, row) => {
           if (err) {
             console.error(err.message);
           }
--- app/server.js
+++ app/server.js
@@ -2,15 +2,18 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const db = require('./db/index.js');
 const router = require('./router/index.js');
-
+const notificationService = require('./services/notificationService.js');
 const auth = require("../auth.json");
+const { SERVER_NAME, CHANNEL_NAME } = require("./config");
+let channel;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  channel = client.guilds.find(g => g.name === SERVER_NAME).channels.find(ch => ch.name === CHANNEL_NAME);
+  notificationService.init(channel);
 });
 
 client.on('message', msg => {
-  const channel = msg.guild.channels.find(ch => ch.name === 'general');
   router(msg, channel);
 });
 
